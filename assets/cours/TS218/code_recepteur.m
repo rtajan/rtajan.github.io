ba1f9605b929e8fe3cbcb8b1bb734e5b351a0e34
Romain Tajan <romain.tajan@bordeaux-inp.fr>
2024-04-12 07:07:44 +0200

    Update code_recepteur.m

--- assets/cours/TS218/code_recepteur.m
+++ assets/cours/TS218/code_recepteur.m
@@ -1,18 +1,23 @@
-clear all
+clear
 close all
 clc
 
-%% Chargement du fichier contenant le signal reçu
+level = 1;
+
+%% Chargement du fichier contenant le signal reÃ§u
 load 'signal_recu.mat';
+if level > 1
+  signal_recu = signal_recu(1:5:end);
+end
 
-%% Votre récepteur
-% En entrée : signal_recu, signal équivalent à rl(kTe) avec Te le temps
-% d'échantillonnage
+%% Votre rÃ©cepteur
+% En entrÃ©e : signal_recu, signal Ã©quivalent Ã  rl(kTe) avec Te le temps
+% d'Ã©chantillonnage
 
-% hatB doit être une matrice de log2(M) = 2 lignes et Ns = 65536 colonnes
+% hatB doit Ãªtre une matrice de log2(M) = 2 lignes et Ns = 65536 colonnes
 % A partir des entiers obtenus en sortie de theMod.modulate_Int, hatB est
-% calculé grace à la fonction de2bi(foo,2);
-%% Décodage de source
+% calculÃ© grace Ã  la fonction de2bi(foo,2);
+%% DÃ©codage de source
 hatMatBitImg = reshape(hatB(:),[],8);
 matImg = bi2de(hatMatBitImg);
 Img = reshape(matImg,128,128);
