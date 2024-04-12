bc47dcda15cd67b08a02e869c20608abf4330d6e
Romain Tajan <romain.tajan@bordeaux-inp.fr>
2024-04-12 07:10:17 +0200

    Update code_recepteur.m

--- assets/cours/TS218/code_recepteur.m
+++ assets/cours/TS218/code_recepteur.m
@@ -14,13 +14,13 @@ end
 % En entrée : signal_recu, signal équivalent à rl(kTe) avec Te le temps
 % d'échantillonnage
 
-% hatB doit être une matrice de log2(M) = 2 lignes et Ns = 65536 colonnes
-% A partir des entiers obtenus en sortie de theMod.modulate_Int, hatB est
-% calculé grace à la fonction de2bi(foo,2);
+% hatB doit être une matrice de log2(M) lignes et Ns
+% calculé grace à la fonction de2bi(foo,2) foo étant ici une représentation entière des étiquettes
 %% Décodage de source
 hatMatBitImg = reshape(hatB(:),[],8);
 matImg = bi2de(hatMatBitImg);
-Img = reshape(matImg,128,128);
+T = 1 % Changer ici la taille de l'image
+Img = reshape(matImg,T,T);
 
 %% Affichage
 figure
